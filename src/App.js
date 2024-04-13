bb0d64b4f7cdf4c0a2addeb066acdec515c0762a
codewithgi <118617546+codewgi@users.noreply.github.com>
2024-04-13 13:23:45 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { Fragment, useEffect, useRef, useState } from 'react';
-import Lenis from '@studio-freight/lenis'
+import Lenis from '@studio-freight/lenis';
+import { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/dist/ScrollTrigger';
 
 function App() {
   const imageRef = useRef([]);
-  const imageContainerRef = useRef([]);
+  const imageContainerRef = useRef();
 
   let imageSection = [];
   for(let i = 1; i <= 7; i++) {
@@ -15,12 +15,12 @@ function App() {
       <div className='w-[100vh] shrink-0 rounded-3xl overflow-hidden' ref={(ref) => imageRef.current[i-1] = ref}>
         <img src={`/img/${i}.jpg`} className='w-full'/>
       </div>
-    );
+    )
   }
-  
+
   // gsap
   gsap.registerPlugin(ScrollTrigger);
-  
+
   useEffect(() => {
     gsap.to(imageRef.current, {
       xPercent : -100 * (imageRef.current.length - 1),
@@ -30,21 +30,21 @@ function App() {
         scrub : 1,
         end : '+=' + imageContainerRef.current.offsetWidth,
         pin : true,
-        snap : 1 / (imageRef.current - 1)
+        snap : 1/(imageRef.current - 1),
       }
     })
-    
+
     return () => {
-      ScrollTrigger.getAll().forEach(st => st.kill())
+      ScrollTrigger.getAll().forEach(st => st.kill());
     }
-  }, [])
+  })
 
   // lenis
   const lenis = new Lenis();
   lenis.on('scroll', () => {});
 
   function raf(time) {
-    lenis.raf(time)
+    lenis.raf(time);
     requestAnimationFrame(raf);
   }
   requestAnimationFrame(raf);
@@ -54,10 +54,10 @@ function App() {
       <section className='min-h-screen flex justify-center items-center'>
         <h1 className='font-bold text-8xl text-white'>Scroll down</h1>
       </section>
-      <section className="min-h-screen w-full flex flex-nowrap items-center space-x-10 px-auto" ref={imageContainerRef} style={{ width : `calc(100vw * 6)` }}>
+      <section className='min-h-screen flex flex-nowrap items-center space-x-10 px-20' ref={imageContainerRef} style={{ width : 'calc(100vw * 6)' }}>
         {imageSection}
       </section>
-      <section className='min-h-screen'></section>
+      <section className='w-full min-h-screen'></section>
     </main>
   );
 }
